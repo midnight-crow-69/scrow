452f018be1194fc1d5a42ab522b26e27187d7bbd
midnight-crow-69 <midnight-crow-69@users.noreply.github.com>
2026-07-15 17:12:06 +0600

    Added Firefox CSS theme and VSCodium settings
    - Firefox Material You theme (userChrome.css, userContent.css)
    - Firefox colors (auto-generated by matugen)
    - Firefox user.js preferences
    - VSCodium settings.json

---
+++ .mozilla/firefox/efypgmxk.default-release/user.js
@@ -0,0 +1,5 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("widget.content.allow-gtk-dark-theme", true);
+user_pref("layout.css.prefers-color-scheme.content", 2);
+user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("devtools.chrome.enabled", true);
