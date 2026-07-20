0dd68ed85e2263a45321518a1b374c3dac90f89b
midnight-crow-69 <midnight-crow-69@users.noreply.github.com>
2026-07-20 21:02:22 +0600

    Updated dotfiles 2026-07-20_21:02

--- .config/hypr/modules/borders.lua
+++ .config/hypr/modules/borders.lua
@@ -1,7 +1,7 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(a1cafdee)",
+            active_border   = "rgba(a6c8ffee)",
             inactive_border = "rgba(c3c6cfaa)",
         },
     },
